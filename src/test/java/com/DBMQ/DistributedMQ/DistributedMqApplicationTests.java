992a3e21187416ef212b3bb48f60f3e26f11387f
FelicxWayne <amalanirmalfelicx@gmail.com>
2026-06-12 18:35:19 +0530

    Updated

---
+++ src/test/java/com/DBMQ/DistributedMQ/DistributedMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBMQ.DistributedMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
