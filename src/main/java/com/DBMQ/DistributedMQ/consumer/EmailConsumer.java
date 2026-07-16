798f89332a1a044cedde25cf43fdbe7b8d675519
FelicxWayne <amalanirmalfelicx@gmail.com>
2026-07-16 13:02:57 +0530

    Implemented EmailConsumer class

--- src/main/java/com/DBMQ/DistributedMQ/consumer/EmailConsumer.java
+++ src/main/java/com/DBMQ/DistributedMQ/consumer/EmailConsumer.java
@@ -1,8 +1,25 @@
 package com.DBMQ.DistributedMQ.consumer;
 
+import com.DBMQ.DistributedMQ.constants.StreamConstants;
+import com.DBMQ.DistributedMQ.model.orderEvent;
+import com.DBMQ.DistributedMQ.service.AckService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.stream.RecordId;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EmailConsumer extends ConsumerBase{
+public class EmailConsumer extends ConsumerBase {
 
+    private static final Logger log = LoggerFactory.getLogger(EmailConsumer.class);
+
+    public EmailConsumer(AckService ackService) {
+        super(StreamConstants.ORDER_PLACED, StreamConstants.EMAIL_GROUP, ackService);
+    }
+
+    @Override
+    protected void process(orderEvent event, RecordId recordId) throws Exception {
+        log.info("[EmailConsumer] Processing email notification for order: {} (userId: {})", 
+                 event.getOrderId(), event.getUserId());
+    }
 }
