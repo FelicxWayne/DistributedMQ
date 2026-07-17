427d8535bf6760c3ca95ed62b80a9fcecbbd4393
Felicx <amalanirmalfelicx@gmail.com>
2026-07-17 13:49:01 +0530

    Update DistributedMqApplication.java

--- src/main/java/com/DBMQ/DistributedMQ/DistributedMqApplication.java
+++ src/main/java/com/DBMQ/DistributedMQ/DistributedMqApplication.java
@@ -3,6 +3,7 @@ package com.DBMQ.DistributedMQ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableScheduling
 @SpringBootApplication
 public class DistributedMqApplication {
 
